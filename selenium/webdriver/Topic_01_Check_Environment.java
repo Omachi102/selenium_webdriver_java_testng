5443821eb63ce9ff6ba7763e185da7cdcb311c28
Omachi <nthloan102@gmail.com>
2021-06-16 22:34:19 +0700

    Create repository

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -12,11 +13,16 @@ import org.testng.annotations.Test;
 
 public class Topic_01_Check_Environment {
 	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
 
 	@BeforeClass
 	public void beforeClass() {
-		System.setProperty("webdriver.gecko.driver",".//browserDrivers/geckodriver");
-		driver = new FirefoxDriver();
+		//System.setProperty("webdriver.gecko.driver",projectPath + "/browserDrivers/geckodriver");
+		//driver = new FirefoxDriver();
+		
+		System.setProperty("webdriver.chrome.driver",projectPath + "/browserChrome/chromedriver");
+		driver = new ChromeDriver();
+		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("http://demo.guru99.com/v4/");
